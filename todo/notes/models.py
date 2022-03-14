9d5a4253067d825de2ce23b9c1096184b386421b
zubun <zubun7@gmail.com>
2022-03-14 23:46:46 +0300

    Lesson 2.admin

---
+++ todo/notes/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from usertodoapp.models import CustomUser
+
+
+class Projects(models.Model):
+    title = models.CharField(max_length=150)
+    link = models.URLField(max_length=200)
+    users = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Todo(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.CharField(max_length=400, blank=True)
+    date_create = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+    done = models.BooleanField(default=False, null=False)
+    project = models.ForeignKey('Projects', on_delete=models.PROTECT, null=True)
+
+    # def __str__(self):
+    #     return self.title
---
+++ todo/notes/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import Todo, Projects
+
+
+# Notes serializer
+class TodoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = '__all__'
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Projects
+        fields = '__all__'
