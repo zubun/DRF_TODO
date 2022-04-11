6c16fd22ffb8e730a92e3b6ecdb14c96520eb52f
zubun <zubun7@gmail.com>
2022-04-11 22:29:48 +0300

    Lesson 7.Не получается.

---
+++ todo/frontend/src/components/Auth.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import {Link, Route} from "react-router-dom";
+import axios from "axios";
+
+class LoginForm extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {login: '', password: ''}
+        }
+    handleChange(event)
+    {
+        this.setState(
+            {
+                      [event.target.name]: event.target.value
+                  }
+        );
+    }
+    // handleSubmit(event) {
+    //     console.log(this.state.login + ' ' + this.state.password)
+    //     event.preventDefault()
+    // }
+    handleSubmit(event) {
+        // console.log(this.state.login + ' ' + this.state.password)
+        this.props.get_token(this.state.login, this.state.password)
+        event.preventDefault()
+    }
+
+
+    render() {
+        return (
+                <form onSubmit={(event)=> this.handleSubmit(event)}>
+                                    <input type="text" name="login" placeholder="login"
+                                        value={this.state.login} onChange={(event)=>this.handleChange(event)} />
+                                    <input type="password" name="password" placeholder="password"
+                                        value={this.state.password} onChange={(event)=>this.handleChange(event)} />
+                                    <input type="submit" value="Login" />
+
+                </form>
+
+        );
+    }
+}
+export default LoginForm
--- todo/manage.py
+++ todo/manage.py
@@ -20,3 +20,9 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+import requests
+
+response = requests.post('http://127.0.0.1:8000/api-token-auth/', data={'username':'Users2', 'password': 'Users2'})
+print(response.status_code)
+print(response.json())
