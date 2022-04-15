c4c52d619b61ad4296d0440d34b4fdc71eb8eaf7
zubun <zubun7@gmail.com>
2022-04-15 20:24:39 +0300

    Lesson 7.Не получается. (#8)
    * Lesson 7.Не получается.
    
    * Lesson 7. Code style fix

--- todo/frontend/src/App.js
+++ todo/frontend/src/App.js
@@ -13,7 +13,8 @@ import ToDoList from "./components/ToDo";
 import UserToDoList from "./components/UserToDo.js";
 
 import * as PropTypes from "prop-types";
-
+import LoginForm from "./components/Auth";
+import Cookies from 'universal-cookie';
 
 // function Menu(props) {
 //     // props.ObjectMen = Men;
@@ -35,45 +36,85 @@ class App extends React.Component{
         this.state = {
             'CustomUser':[],
             'Project':[],
-            'ToDo':[]
+            'ToDo':[],
+            'token':'635b6fbb5174e1b6dc5d767e871da05761370933',
             // 'ObjectMen':[]
 
         }
+        // this.logout = this.logout.bind(this);
     }
-    componentDidMount() {
-      // const CustomUser = [
-      //     {
-      //         'first_name': 'Фёдор',
-      //         'last_name': 'Достоевский',
-      //         'email': '1821@ya.ru'
-      //     },
-      //     {
-      //         'first_name': 'Александр',
-      //         'last_name': 'Грин',
-      //         'email': '1880@ya.ru'
-      //     },
-      // ]
-      axios.get('http://127.0.0.1:8000/api/users/').then(response => {
-          this.setState(
-              {
-                  'CustomUser': response.data
-              }
-          )}).catch(error => console.log(error))
+    set_token(token) {
+        const cookies = new Cookies()
+        cookies.set('token', token)
+        this.setState({'token': token})
+    }
 
-      axios.get('http://127.0.0.1:8000/api/Project/').then(response => {
-          this.setState(
-              {
-                  'Projects': response.data
-              }
-          )}).catch(error => console.log(error))
+    is_authenticated() {
+        return this.state.token !== ''
+    }
 
-      axios.get('http://127.0.0.1:8000/api/Todo/').then(response => {
-          this.setState(
-              {
-                  'ToDo': response.data
-              }
-          )}).catch(error => console.log(error))
+    logout() {
+        this.set_token('')
+    }
+
+    get_token_from_storage() {
+        const cookies = new Cookies()
+        const token = cookies.get('token')
+        this.setState({'token': token})
+    }
+
+    get_token(username, password) {
+       axios.post('http://127.0.0.1:8000/api-token-auth/',
+                {'username': username, 'password': password})
+           .then(response => {
+               this.set_token(response.data['token'])
+           }).catch(error => alert('Неверный логин или пароль'))
+
    }
+
+    load_data() {
+        // const CustomUser = [
+        //     {
+        //         'first_name': 'Фёдор',
+        //         'last_name': 'Достоевский',
+        //         'email': '1821@ya.ru'
+        //     },
+        //     {
+        //         'first_name': 'Александр',
+        //         'last_name': 'Грин',
+        //         'email': '1880@ya.ru'
+        //     },
+        // ]
+        axios.get('http://127.0.0.1:8000/api/users/').then(response => {
+            this.setState(
+                {
+                    'CustomUser': response.data
+                }
+            )
+        }).catch(error => console.log(error))
+
+        axios.get('http://127.0.0.1:8000/api/Project/').then(response => {
+            this.setState(
+                {
+                    'Projects': response.data
+                }
+            )
+        }).catch(error => console.log(error))
+
+        axios.get('http://127.0.0.1:8000/api/Todo/').then(response => {
+            this.setState(
+                {
+                    'ToDo': response.data
+                }
+            )
+        }).catch(error => console.log(error))
+    }
+
+    componentDidMount() {
+      this.get_token_from_storage()
+      this.load_data()
+   }
+
    render () {
        return (
             <React.Fragment>
@@ -85,6 +126,9 @@ class App extends React.Component{
                 <Route exact path='/Projects' component={() => <ProjectsList Projects={this.state.Projects} />} />
                 <Route exact path='/ToDo' component={() => <ToDoList ToDo={this.state.ToDo} />} />
                 <Route exact path='/ToDoProject' component={() => <ToDoList ToDo={this.state.ToDo} />} />
+
+                <Route exact path='/login' component={() => <LoginForm
+                    get_token={(username, password) => this.get_token(username, password)} />} />
 
                 <Route path='/userlist/:id'>
                     <UserToDoList Todo={this.state.ToDo} />}
--- todo/frontend/src/components/menu.js
+++ todo/frontend/src/components/menu.js
@@ -2,7 +2,16 @@ import React from 'react'
 import {BrowserRouter, Route, Link} from 'react-router-dom'
 
 const Menu = ({ObjectMen}) => {
-   return (
+   let login_button = ''
+
+    if (this.is_authenticated()) {
+    login_button = <button type="button" class="btn btn-danger" onClick={this.logout}>Hello, {this.CustomUser.username} Logout</button>
+    }
+    else {
+      login_button = <Link to='/login' class="btn btn-danger">Login</Link>
+    }
+
+    return (
 
            <ul class="nav nav-tabs">
                <li class="nav-item">
@@ -24,6 +33,10 @@ const Menu = ({ObjectMen}) => {
                 <li>
                     <Link className="nav-link active" data-bs-toggle="tab" to="/">Контакты</Link>
                </li>
+
+                   {login_button}
+
+
                <li>
                     <Link className="nav-link active" data-bs-toggle="tab" to="/ToDoProject">ghg</Link>
                </li>
